bf9eb185713ccc6263a71df71cf6c330f78f23a8
eth <thisiseth@users.noreply.github.com>
2024-08-20 16:48:13 +0400

    .

--- src/esp32s3/chocolate-doom/main/main.c
+++ src/esp32s3/chocolate-doom/main/main.c
@@ -23,15 +23,42 @@ static wl_handle_t flash_rw_wl_handle = WL_INVALID_HANDLE;
 
 static esp_partition_mmap_handle_t wad_mmap_handle;
 static const void *wad_mmap;
-    
-void user_task(void *arg)
+
+static void abort_with_error_led()
+{
+#ifndef PMOD_OCTAL_SPI_IN_USE
+    pmod_esp32s3_led_set_green(false);
+    pmod_esp32s3_led_set_pink(true);
+#else
+    pmod_esp32s3_led_set_rgb(12, 0, 0);
+#endif
+
+    abort();
+}
+
+static void loading_led()
+{
+#ifndef PMOD_OCTAL_SPI_IN_USE
+    pmod_esp32s3_led_set_green(true);
+    pmod_esp32s3_led_set_pink(true);
+#else
+    pmod_esp32s3_led_set_rgb(4, 2, 0);
+#endif
+}
+
+static void running_led()
 {
 #ifndef PMOD_OCTAL_SPI_IN_USE
     pmod_esp32s3_led_set_green(true);
     pmod_esp32s3_led_set_pink(false);
 #else
-    pmod_esp32s3_led_set_rgb(0, 4, 0);
+    pmod_esp32s3_led_set_rgb(0, 2, 0);
 #endif
+}
+    
+void user_task(void *arg)
+{
+    running_led();
 
     doom_main("/"ESP32_DOOM_WAD_NAME, ESP32_DOOM_WAD_SIZE, wad_mmap);
 
@@ -43,16 +70,6 @@ void user_task(void *arg)
     vTaskDelete(NULL);
 }
 
-static void error_led()
-{
-#ifndef PMOD_OCTAL_SPI_IN_USE
-    pmod_esp32s3_led_set_green(false);
-    pmod_esp32s3_led_set_pink(true);
-#else
-    pmod_esp32s3_led_set_rgb(16, 0, 0);
-#endif
-}
-
 void app_main(void)
 {
     if (!pmod_esp32s3_init())
@@ -61,12 +78,7 @@ void app_main(void)
         return;
     }
 
-#ifndef PMOD_OCTAL_SPI_IN_USE
-    pmod_esp32s3_led_set_green(true);
-    pmod_esp32s3_led_set_pink(true);
-#else
-    pmod_esp32s3_led_set_rgb(8, 4, 0);
-#endif
+    loading_led();
 
     fpga_driver_config_t driver_config = 
     {
@@ -82,8 +94,7 @@ void app_main(void)
     if (!fpga_driver_init(&driver_config))
     {
         ESP_LOGE(TAG, "failed to init driver");
-        error_led();
-        return;
+        abort_with_error_led();
     }
 
     ESP_LOGI(TAG, "waiting for fpga...");
@@ -107,8 +118,7 @@ void app_main(void)
     if (err != ESP_OK) 
     {
         ESP_LOGE(TAG, "Failed to mount FATFS (%s)", esp_err_to_name(err));
-        error_led();
-        return;
+        abort_with_error_led();
     }
 
     const esp_partition_t *partition = esp_partition_find_first(ESP_PARTITION_TYPE_DATA, ESP_PARTITION_SUBTYPE_DATA_UNDEFINED, "wads");
@@ -116,20 +126,18 @@ void app_main(void)
     if (partition == NULL) 
     {
         ESP_LOGE(TAG, "Failed to find wads partition");
-        error_led();
-        return;
+        abort_with_error_led();
     }
 
     if (esp_partition_mmap(partition, 0, ESP32_DOOM_WAD_SIZE, ESP_PARTITION_MMAP_DATA, &wad_mmap, &wad_mmap_handle) != ESP_OK)
     {
         ESP_LOGE(TAG, "Failed to mmap wads partition");
-        error_led();
-        return;
+        abort_with_error_led();
     }
 
     if (xTaskCreatePinnedToCore(user_task, "user_task", 16384, NULL, tskIDLE_PRIORITY+1, NULL, 1) != pdPASS)
     {
         ESP_LOGE(TAG, "failed to start user task");
-        error_led();
+        abort_with_error_led();
     }
 }
