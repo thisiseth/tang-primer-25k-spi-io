7e0693ab244b30787833b6bfa95ac3bf4f6f9d41
eth <thisiseth@users.noreply.github.com>
2024-08-28 20:10:42 +0400

    pesky tabs

--- src/esp32s3/quake/components/quake/fatfs_proxy.h
+++ src/esp32s3/quake/components/quake/fatfs_proxy.h
@@ -8,15 +8,15 @@
 void fatfs_proxy_init(TaskHandle_t ownerTask);
 
 FILE* fatfs_proxy_fopen(const char * restrict name, const char * restrict type);
-int	fatfs_proxy_fclose(FILE *file);
-int	fatfs_proxy_fseek(FILE *file, long pos, int type);
+int fatfs_proxy_fclose(FILE *file);
+int fatfs_proxy_fseek(FILE *file, long pos, int type);
 long fatfs_proxy_ftell(FILE *file);
 size_t fatfs_proxy_fread(void* restrict buf, size_t size, size_t n, FILE* restrict file);
 size_t fatfs_proxy_fwrite(const void* restrict buf, size_t size, size_t n, FILE *file);
-int	fatfs_proxy_vfprintf(FILE* restrict file, const char* restrict fmt, va_list args);
-int	fatfs_proxy_vfscanf(FILE* restrict file, const char* restrict fmt, va_list args);
-int	fatfs_proxy_fgetc(FILE *file);
-int	fatfs_proxy_fflush(FILE *file);
-int	fatfs_proxy_feof(FILE *file);
+int fatfs_proxy_vfprintf(FILE* restrict file, const char* restrict fmt, va_list args);
+int fatfs_proxy_vfscanf(FILE* restrict file, const char* restrict fmt, va_list args);
+int fatfs_proxy_fgetc(FILE *file);
+int fatfs_proxy_fflush(FILE *file);
+int fatfs_proxy_feof(FILE *file);
 
-int	fatfs_proxy_mkdir(const char *path, mode_t mode);
+int fatfs_proxy_mkdir(const char *path, mode_t mode);
